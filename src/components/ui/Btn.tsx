1951586889ab0d11fc3a53a5588db87132d21604
Abdullahkhan871 <akhan283785@gmail.com>
2025-09-03 15:25:29 +0530

    feat: add Btn component with user icon and placeholder spans And remove some css properties

---
+++ src/components/ui/Btn.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { User } from "phosphor-react";
+const Btn = () => {
+    return (
+        <button>
+            {/* user icon */}
+            <span>
+
+            </span>
+            {/* text  */}
+            <span>
+
+            </span>
+            {/* arrow right */}
+            <span>
+                <User size={32} color='#fff' />
+            </span>
+        </button>
+    )
+}
+
+export default Btn
